0335339fac5c7c9f91fd051f94c10a2543f4f58d
Daniel <christopher.daniel@aun.edu.ng>
2020-02-14 13:40:10 +0100

    expo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,20 +1,58 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Register from "./register";
-import { Container, Table} from 'reactstrap';
+import React, {useState,Fragment } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Modal, ModalHeader,Button,
+ModalBody, ModalFooter} from 'reactstrap';
 
 
-class Home extends Component {
-    state = {  }
-    render() { 
+const Home = (props) => {
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
+  
         return ( <Fragment>
+           <div>
+     
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Register as:</ModalHeader>
+        <ModalBody>        
+          <div style={{width:"100%", display:"flex", flex:"wrapFlex",marginLeft:"auto",marginRight:"auto"}}>
+          <div style={{width:"40%", marginLeft:"auto",marginRight:"auto",padding:"2rem", background:"#fff",boxShadow:"0 5px 45px 0 rgba(89, 109, 121, 0.2"}}>
+          <a style={{width:"100%"}} href="https://forms.gle/wTP4ZWxwPRjHCgto7" target="blank" >
+             <div className="join-now-btn-modal">
+             <span style={{ color:"blue"}}>Individual</span>{" "}
+              <i style={{ marginLeft: "0.6rem" }} class="material-icons">
+              perm_identity
+              </i>
+            </div></a>
+            </div>
+            <div style={{width:"40%",marginLeft:"auto",marginRight:"auto", padding:"2rem",background:"#fff",boxShadow:"0 5px 45px 0 rgba(89, 109, 121, 0.2"}}>
+            <a href="https://forms.gle/eC2DY55XvVSTTy4J9" target="blank"> <div className="join-now-btn-modal">
+             <span style={{ color:"red"}}>Corporate</span>{" "}
+              <i style={{ marginLeft: "0.6rem" }} class="material-icons">
+              people_outline
+              </i>
+            </div></a>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
             <Router>
            <div className="background1">
            <div className="title-div" style={{display:"flex", width:"100%", height:"450px",marginLeft:"auto",marginRight:"auto"}}>
            <div className="reg-but" style={{paddingTop:"5rem",marginTop:"3rem",width:"70%",marginLeft:"auto",marginRight:"auto"}}>
     <h2 className="icascie-3" style={{background:"#000", fontSize:"21px"}}> PARTICIPATE AT THE SCIENCE, TECHNOLOGY AND INNOVATION EXPO' 2020</h2>
     <h3 className="icascie-3" style={{marginTop:"2rem"}}><em></em></h3>
-    <h1 className="icascie-2" style={{background:""}}>Enhancing The Growth Of A Diversified Economy Through Science And Technology</h1>
+    <h1 className="icascie-2" style={{background:""}}> Enhancing The Growth Of A Diversified Economy Through Science And Technology</h1>
 </div>
 
 
@@ -32,7 +70,8 @@ class Home extends Component {
             
      <p style={{textAlign:"justify", fontSize:"18px"}} >The American University of Nigeria is organizing
      a Technology and Innovation Expo'2020 to facilitate commercialization of Research and Development results
-     and encourage inventions and innovations in the state.<br/>
+     and encourage inventions and innovations in the state. The Theme for the Expo is
+     <strong> Enhancing The Growth Of A Diversified Economy Through Science And Technology</strong>.<br/>
      The objectives of the Expo are to: <br/>
      <ul>
         <li>Encourage interactions between Researchers and Investors with a view to commercializing
@@ -47,10 +86,11 @@ class Home extends Component {
     </div>
 
     <div className="reg-but" style={{marginTop:"1rem",width:"30%",marginLeft:"auto",marginRight:"auto"}}>
+   <div style={{animation: "fadeIn 13.25s infinite"}}>
    <h3>Submission Dates</h3>
    <p><em>Nothing to display</em></p>
-
-    <a target="blank" href="https://docs.google.com/forms/d/10IReUP-vV8QTKpehTKzyzVMdwqKxDkGklx_HOWz-irk/edit"> <div className="join-now-btn">
+   </div>
+   <a onClick={toggle} target="blank"> <div className="join-now-btn">
              <span style={{ marginLeft: "1.2rem" ,color:"red"}}>REGISTER HERE</span>{" "}
               <i style={{ marginLeft: "0.6rem" }} class="material-icons">
                 arrow_forward
@@ -61,11 +101,11 @@ class Home extends Component {
 
 
 
-<Route path="/register_now"  component={Register}/>
+
 
 </Router>
         </Fragment> );
-    }
+    
 }
  
 export default Home;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,7 @@ import React, { Fragment, Component,useState } from 'react';
 import aunLogo from "../assets/aun-logo-top.png";
 import background1 from "../assets/slanted.svg";
 import Home from "./home";
-import Register from "./register";
-import Submission from "./submission";
-import Symposium from "./symposium";
-import Venue from "./venue";
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {
   Container,
@@ -39,17 +36,21 @@ const NavbarComponent = (props) => {
       <Navbar style={{background:"#030306"}} expand="md">
         <NavbarBrand href="/" style={{width:"100%"}}>               
          <a href="/" title="aun logo" rel="home">
-                 <center style={{width:"100%"}}> <img
+                 <div style={{width:"100%"}}> 
+                 <img
                     style={{                     
-                      height : "40px",
-                      width : "120px"                      
+                      height : "70px",
+                    
+                     padding:"1rem" 
+                                        
                     }}
                     src={aunLogo}
                     alt="aun-logo"
-                  /></center>
+                  />
+                  </div>
                 </a>
         </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
+      
         
       </Navbar>
  </div>
@@ -57,10 +58,6 @@ const NavbarComponent = (props) => {
 </div>
 
 <Route path="/" exact component={Home}/>
-<Route path="/register_now"  component={Register}/>
-<Route path="/submission"  component={Submission}/>
-<Route path="/symposium"  component={Symposium}/>
-<Route path="/venue&amp;accomodation"  component={Venue}/>
 </Router>
 </Fragment> );
     
